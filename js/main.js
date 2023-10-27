ad32e1998bed607f3f46f1328c96d73e76eef0df
Camilla <camillagammeri@gmail.com>
2023-10-27 21:07:11 +0200

    Update: emailjs updated (submit to be fixed)

--- js/main.js
+++ js/main.js
@@ -60,14 +60,23 @@ document.addEventListener('scroll', () => {
 // EMAILJS
 
 function sendMail() {
-
   var params = {
-    name: document.getElementById("#name").value,
-    email: document.getElementById("#email").value,
-    message: document.getElementById("#message").value,
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
   };
 
-  emailjs.send("service_kax7wo9", "template_zahmm6e", params).then(function (res) {
-    alert("Success" + res.status);
+const serviceID = "service_kax7wo9";
+const templateID = "template_zahmm6e";
+
+emailjs
+  .send(serviceID, templateID, params)
+  .then((res) => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("Your message sent successfully!!");
   })
+  .catch((err) => console.log(err));
 }
